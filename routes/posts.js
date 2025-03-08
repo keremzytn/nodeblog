9f4ab096bd203984dbe0adfb12368fbf605cd0ad
Mustafa Kerem Zeytun <mustafakeremzeytun1@gmail.com>
2025-03-08 19:35:00 +0300

    bloglar için search kısmı eklendi

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,6 @@ const path = require('path')
 const Category = require('../models/Category')
 const User = require('../models/User')
 
-
 router.get('/new', (req, res) => {
     if(!req.session.userId){
         res.redirect('/users/login')
@@ -14,7 +13,70 @@ router.get('/new', (req, res) => {
     Category.find({}).then(categories =>{
         res.render('site/addpost', {categories: categories})
     })
-})
+}) 
+
+router.get('/search', (req, res) => {
+    if (req.query.look) {
+        const regex = new RegExp(escapeRegex(req.query.look), 'gi');
+        Post.find({ "title": regex }).populate({ path: 'author', model: User }).sort({ $natural: -1 }).then(posts => {
+            Category.aggregate([
+                {
+                    $lookup: {
+                        from: 'posts',
+                        localField: '_id',
+                        foreignField: 'category',
+                        as: 'posts'
+                    }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        name: 1,
+                        num_of_posts: { $size: '$posts' }
+                    }
+                },
+                {
+                    $sort: { _id: -1 }
+                }
+            ]).then(categoryAggregated => { // categories yerine categoryAggregated kullanıldı
+                Post.find({}).populate({ path: 'author', model: User }).sort({ $natural: -1 }).then(allPosts => { // allPosts oluşturuldu
+                    res.render('site/blog', { categories: categoryAggregated, posts: posts, categoryAggregated: categoryAggregated, allPosts: allPosts }); // categoryAggregated ve allPosts gönderiliyor
+                });
+            });
+        });
+    } else {
+        Post.find({}).populate({ path: 'author', model: User }).sort({ $natural: -1 }).then(posts => {
+            Category.aggregate([
+                {
+                    $lookup: {
+                        from: 'posts',
+                        localField: '_id',
+                        foreignField: 'category',
+                        as: 'posts'
+                    }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        name: 1,
+                        num_of_posts: { $size: '$posts' }
+                    }
+                },
+                {
+                    $sort: { _id: -1 }
+                }
+            ]).then(categoryAggregated => {
+                Post.find({}).populate({ path: 'author', model: User }).sort({ $natural: -1 }).then(allPosts => {
+                    res.render('site/blog', { categories: categoryAggregated, posts: posts, categoryAggregated: categoryAggregated, allPosts: allPosts });
+                });
+            });
+        });
+    }
+});
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
 
 router.get('/category/:categoryId', (req, res) => {
     Promise.all([
@@ -62,33 +124,33 @@ router.get('/category/:categoryId', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-        Promise.all([
-            Category.find({}).sort({ $natural: -1 }),
-            Post.findById(req.params.id).populate({path:'author', model: User}).sort({ $natural: -1 }),
-            Post.find({}).populate({path:'author', model: User}).sort({ $natural: -1 }),
-            Category.aggregate([
-                            {
-                                $lookup: {
-                                    from: 'posts',
-                                    localField: '_id',
-                                    foreignField: 'category',
-                                    as: 'posts'
-                                }
-                            },
-                            {
-                                $project: {
-                                    _id: 1,
-                                    name: 1,
-                                    num_of_posts: { $size: '$posts' }
-                                }
-                            },
-                            {
-                                $sort: { _id: -1 }
-                            }
-                        ])
+    Promise.all([
+        Category.find({}).sort({ $natural: -1 }),
+        Post.findById(req.params.id).populate({ path: 'author', model: User }).sort({ $natural: -1 }),
+        Post.find({}).populate({ path: 'author', model: User }).sort({ $natural: -1 }), // posts -> allPosts
+        Category.aggregate([
+            {
+                $lookup: {
+                    from: 'posts',
+                    localField: '_id',
+                    foreignField: 'category',
+                    as: 'posts'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    num_of_posts: { $size: '$posts' }
+                }
+            },
+            {
+                $sort: { _id: -1 }
+            }
         ])
-        .then(([categories, post, posts, categoryAggregated]) => {
-            res.render('site/post', { categories: categories, post: post , posts: posts, categoryAggregated: categoryAggregated});
+    ])
+        .then(([categories, post, allPosts, categoryAggregated]) => { // posts -> allPosts
+            res.render('site/post', { categories: categories, post: post, allPosts: allPosts, categoryAggregated: categoryAggregated }); // posts -> allPosts
         })
         .catch(error => {
             console.error("Veri çekme hatası:", error);
